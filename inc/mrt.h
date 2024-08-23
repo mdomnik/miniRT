d565677b1e7915b239b22b5dd8224a0978df21e3
tesla33io <artemstavropolov@gmail.com>
2024-08-23 10:42:59 +0200

    Chore: modify main and main header

--- inc/mrt.h
+++ inc/mrt.h
@@ -6,11 +6,20 @@
 /*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 20:06:22 by astavrop          #+#    #+#             */
-/*   Updated: 2024/08/19 20:06:26 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/08/22 20:45:52 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MRT_H
 # define MRT_H
+
+# define ASPECT_R 1.77777777777
+# define IWIDTH 400
+
+# include <stdio.h>
+
+// Define the DEBUG macro
+#define DEBUG(fmt, ...) \
+    fprintf(stderr, "[DEBUG] %s:%d:%s(): " fmt "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
 #endif /* MRT_H */
