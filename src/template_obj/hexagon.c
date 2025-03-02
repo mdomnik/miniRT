b6e517bf960ffe194dc4b5fadee722bb4bfb9f10
tesla33io <artemstavropolov@gmail.com>
2025-03-02 17:04:57 +0100

    Chore: norminette `src/template_obj`

--- src/template_obj/hexagon.c
+++ src/template_obj/hexagon.c
@@ -12,29 +12,36 @@
 
 #include "mrt.h"
 
-static t_shape *hexagon_corner(void)
+static t_shape	*hexagon_corner(void)
 {
-	t_shape *corner;
+	t_shape	*corner;
 
 	corner = sphere();
-	set_transform(corner, multiply_matrices(translation(0, 0, -1), scaling(0.25, 0.25, 0.25)));
+	set_transform(corner, multiply_matrices(
+			translation(0, 0, -1), scaling(0.25, 0.25, 0.25)));
 	return (corner);
 }
 
-static t_shape *hexagon_edge(void)
+static t_shape	*hexagon_edge(void)
 {
-	t_shape *edge;
+	t_shape		*edge;
+	t_matrix	transform;
+	t_matrix	rotation;
+	t_matrix	temp;
 
 	edge = cylinder();
 	edge->size_cap.min = 0;
 	edge->size_cap.max = 1;
-	set_transform(edge, multiply_matrices(multiply_matrices(translation(0, -1, -1), rotation_y(-M_PI / 6)), multiply_matrices(rotation_z(-M_PI / 2), scaling(0.25, 1, 0.25))));
+	rotation = multiply_matrices(rotation_y(-M_PI / 6), rotation_z(-M_PI / 2));
+	temp = multiply_matrices(translation(0, -1, -1), rotation);
+	transform = multiply_matrices(temp, scaling(0.25, 1, 0.25));
+	set_transform(edge, transform);
 	return (edge);
 }
 
-static t_shape *hexagon_side(void)
+static t_shape	*hexagon_side(void)
 {
-	t_shape *side;
+	t_shape	*side;
 
 	side = group();
 	add_child(side, hexagon_corner());
@@ -42,16 +49,17 @@ static t_shape *hexagon_side(void)
 	return (side);
 }
 
-t_shape *hexagon(void)
+t_shape	*hexagon(void)
 {
-	t_shape *hex;
-	int n;
+	t_shape	*hex;
+	t_shape	*side;
+	int		n;
 
 	hex = group();
 	n = 6;
-	while(n > 0)
+	while (n > 0)
 	{
-		t_shape *side = hexagon_side();
+		side = hexagon_side();
 		set_transform(side, rotation_y(n * M_PI / 3));
 		add_child(hex, side);
 		n--;
--- src/template_obj/ice_cream.c
+++ src/template_obj/ice_cream.c
@@ -12,70 +12,86 @@
 
 #include "mrt.h"
 
-t_shape *ice_cream_cone(void)
+#define CONE 0
+#define SCOOP1 1
+#define SCOOP2 2
+#define SCOOP3 3
+
+static t_pattern	*create_cone_texture(void)
 {
-	t_shape *c;
-	t_shape *scoop;
-	t_shape *scoop2;
-	t_shape *scoop3;
-	t_shape *cone_scoop;
+	t_canvas	*waffle;
+	t_pattern	*waffle_pattern;
+	t_pattern	*cone_texture;
 
-	t_canvas *waffle = canvas_from_ppm("textures/waffle.ppm");
-	t_pattern *waffle_pattern = uv_image(waffle);
-	t_pattern *cone_texture = texture_map(waffle_pattern, cylindrical_map);
-
+	waffle = canvas_from_ppm("textures/waffle.ppm");
+	waffle_pattern = uv_image(waffle);
+	cone_texture = texture_map(waffle_pattern, cylindrical_map);
 	set_pattern_transform(cone_texture, scaling(2, 2, 2));
-	c = cone();
-	set_transform(c, multiply_matrices(translation(0, -1, 0), multiply_matrices(rotation_z(M_PI), scaling(0.5, 2, 0.5))));
+	return (cone_texture);
+}
+
+static void	setup_cone_material(t_shape *c, t_pattern *cone_texture)
+{
 	c->material.color = new_color3(1, 0.5, 0.5);
 	c->material.pattern = cone_texture;
-	c->material.bump_map = bump_map_from_ppm("textures/waffle_bump.ppm", 2, cylindrical_map);
+	c->material.bump_map = bump_map_from_ppm("textures/waffle_bump.ppm",
+			2, cylindrical_map);
 	c->material.diffuse = 0.7;
 	c->material.specular = 0.3;
 	c->material.shininess = 200.0;
 	c->material.reflective = 0.2;
 	c->material.transparency = 0.0;
 	c->material.refractive_index = 1.0;
+}
 
+static t_pattern	*create_scoop_texture(void)
+{
+	t_canvas	*scoop_look;
+	t_pattern	*pattern;
+	t_pattern	*texture;
 
-	t_canvas *scoop_look = canvas_from_ppm("textures/choc_scoop.ppm");
-	t_pattern *scoop_look_pattern = uv_image(scoop_look);
-	t_pattern *scoop_texture = texture_map(scoop_look_pattern, cylindrical_map);
+	scoop_look = canvas_from_ppm("textures/choc_scoop.ppm");
+	pattern = uv_image(scoop_look);
+	texture = texture_map(pattern, cylindrical_map);
+	return (texture);
+}
 
-	scoop = sphere();
-	set_transform(scoop, multiply_matrices(translation(0, 2, 0),scaling(0.5, 0.5, 0.5)));
-	scoop->material.color = new_color3(0.4, 0.8, 0.4);
-	scoop->material.pattern = scoop_texture;
-	scoop->material.bump_map = bump_map_from_ppm("textures/choc_scoop_bump.ppm", 1, spherical_map);
+static void	setup_scoop(t_shape *scoop, t_pattern *texture,
+		float y_pos, t_color3 color)
+{
+	set_transform(scoop, multiply_matrices(translation(0, y_pos, 0),
+			scaling(0.5, 0.5, 0.5)));
+	scoop->material.color = color;
+	scoop->material.pattern = texture;
+	scoop->material.bump_map = bump_map_from_ppm(
+			"textures/choc_scoop_bump.ppm", 5, spherical_map);
 	scoop->material.diffuse = 0.7;
 	scoop->material.specular = 0.3;
 	scoop->material.shininess = 200.0;
 	scoop->material.reflective = 0.2;
+}
 
-	scoop2 = sphere();
-	set_transform(scoop2, multiply_matrices(translation(0, 2.7, 0),scaling(0.5, 0.5, 0.5)));
-	scoop2->material.color = new_color3(1, 0.2, 0.9);
-	scoop2->material.pattern = scoop_texture;
-	scoop2->material.bump_map = bump_map_from_ppm("textures/choc_scoop_bump.ppm", 5, spherical_map);
-	scoop2->material.diffuse = 0.7;
-	scoop2->material.specular = 0.3;
-	scoop2->material.shininess = 200.0;
-	scoop2->material.reflective = 0.2;
+t_shape	*ice_cream_cone(void)
+{
+	t_shape		*shapes[4];
+	t_shape		*gr;
+	t_pattern	*texture;
 
-	scoop3 = sphere();
-	set_transform(scoop3, multiply_matrices(translation(0, 3.4, 0),scaling(0.5, 0.5, 0.5)));
-	scoop3->material.color = new_color3(0.2, 0.6, 0.9);
-	scoop3->material.pattern = scoop_texture;
-	scoop3->material.bump_map = bump_map_from_ppm("textures/choc_scoop_bump.ppm", 5, spherical_map);
-	scoop3->material.diffuse = 0.7;
-	scoop3->material.specular = 0.3;
-	scoop3->material.shininess = 200.0;
-	scoop3->material.reflective = 0.2;
-
-	cone_scoop = group();
-	add_child(cone_scoop, c);
-	add_child(cone_scoop, scoop);
-	add_child(cone_scoop, scoop2);
-	add_child(cone_scoop, scoop3);
-	return (cone_scoop);
+	shapes[CONE] = cone();
+	set_transform(shapes[CONE], multiply_matrices(translation(0, -1, 0),
+			multiply_matrices(rotation_z(M_PI), scaling(0.5, 2, 0.5))));
+	setup_cone_material(shapes[CONE], create_cone_texture());
+	texture = create_scoop_texture();
+	shapes[SCOOP1] = sphere();
+	shapes[SCOOP2] = sphere();
+	shapes[SCOOP3] = sphere();
+	setup_scoop(shapes[SCOOP1], texture, 2.0, new_color3(0.4, 0.8, 0.4));
+	setup_scoop(shapes[SCOOP2], texture, 2.7, new_color3(1, 0.2, 0.9));
+	setup_scoop(shapes[SCOOP3], texture, 3.4, new_color3(0.2, 0.6, 0.9));
+	gr = group();
+	add_child(gr, shapes[CONE]);
+	add_child(gr, shapes[SCOOP1]);
+	add_child(gr, shapes[SCOOP2]);
+	add_child(gr, shapes[SCOOP3]);
+	return (gr);
 }
